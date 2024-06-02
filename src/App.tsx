1ab84b89f70d8774d4bdd78e76f3f1216d4709c6
Erkin Ünlü <erkinun@gmail.com>
2024-06-02 18:36:44 +0100

    stop and restart timer

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Timer } from "./components/Timer";
 // TODO add different timers, store them, add different timer seconds and minutes options
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
+    <main className="flex flex-col items-center justify-center h-screen bg-black text-white">
       <h1 className="text-red-500">Hello Timers</h1>
       <Timer />
     </main>
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,22 +5,21 @@ type TimerProps = {
   minutes?: number;
 };
 
-// TODO add continue timer button
-// TODO add good styling, maybe a clock face?
+// 1 add a good timer digital face, with a nice font that shows remaning minutes:seconds:milliseconds
+// 2 TODO add good styling, maybe a clock face, start with a radial div turning to the remaining ratio, like the apple timer
 export function Timer({ seconds = 30 }: TimerProps) {
   // TODO add minutes and maybe hours later on
   // TODO show in milliseconds maybe?
 
   // TODO would these get updated if we didn't use state, it shouldn't but if the component gets re rendered, it would
-
+  const [stopped, setStopped] = useState(false);
   const [remaining, setRemaining] = useState(seconds);
-  const [endTime] = useState(Date.now() + seconds * 1000);
+  const [endTime, setEndTime] = useState(Date.now() + seconds * 1000);
   const intervalIdRef = useRef<number | null>(null);
 
   const checkTimer = () => {
     const now = Date.now();
     if (now >= endTime) {
-      console.log("we should be here?");
       setRemaining(0);
       clearInterval(intervalIdRef.current ?? undefined);
     } else {
@@ -30,20 +29,35 @@ export function Timer({ seconds = 30 }: TimerProps) {
 
   const stopTimer = () => {
     clearInterval(intervalIdRef.current ?? undefined);
+    setStopped(true);
+  };
+
+  const restartTimer = () => {
+    setEndTime(Date.now() + remaining * 1000);
+    setStopped(false);
+    // const intervalId = setInterval(checkTimer, 100);
+    // intervalIdRef.current = intervalId;
   };
 
   useEffect(() => {
-    // set an interval for 100ms
-    const intervalId = setInterval(checkTimer, 100);
-    intervalIdRef.current = intervalId;
-    return () => clearInterval(intervalId);
-  }, [seconds]);
+    console.log({ remaining, stopped, intervalId: intervalIdRef.current });
+    if (!stopped) {
+      // set an interval for 100ms
+      const intervalId = setInterval(checkTimer, 100);
+      intervalIdRef.current = intervalId;
+      return () => clearInterval(intervalId);
+    }
+  }, [seconds, remaining, stopped]);
 
   return (
     <div>
       <div>Timer for {seconds} seconds</div>
       <div>Remaining: {remaining}</div>
-      <button onClick={stopTimer}>Stop timer</button>
+      {stopped ? (
+        <button onClick={restartTimer}>Restart timer</button>
+      ) : (
+        <button onClick={stopTimer}>Stop timer</button>
+      )}
     </div>
   );
 }
