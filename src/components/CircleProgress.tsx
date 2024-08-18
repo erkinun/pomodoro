7fe7c3ee3f0373a20160c2226be2f0f2e841bf05
Erkin Ünlü <erkinun@gmail.com>
2024-08-18 17:06:06 +0100

    fix the way the progress bar moves

--- src/components/CircleProgress.tsx
+++ src/components/CircleProgress.tsx
@@ -6,7 +6,7 @@ type CircleProgressBarProps = {
 // TODO add the timer face here? find good colours to go with black
 // TODO calculate
 export function CircleProgressBar({ remaining, full }: CircleProgressBarProps) {
-  const percentage = `${(remaining / full) * 100}%`;
+  const percentage = `${100 - (remaining / full) * 100}%`;
   return (
     <div
       className="grid place-items-center w-16 m-4 aspect-square progress-bar rounded-full after:content-[attr(aria-valuenow)] after:bg-slate-500 after:w-12 after:aspect-square after:grid after:rounded-full after:place-items-center"
