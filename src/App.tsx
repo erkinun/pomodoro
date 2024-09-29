e71d62dfed09675de3e39c158f0b08c1628137b4
Erkin Ünlü <erkinun@gmail.com>
2024-09-29 17:59:10 +0100

    enter to start timer

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,8 @@ type TimerDefinition = {
   length: TimerProps;
 };
 
-// TODO add the enter key to add a timer
 // TODO persist old timers in localstorage
 // TODO come up with a nice timer selector component like 2 hours 4 minutes 36 seconds stuff, maybe circular selection like apple?
-// TODO enter button at top input for timer adds timer
 // TODO sound off configuration
 // TODO style in canva or figma later on
 // TODO add different timers, store them, add different timer seconds and minutes options, localstorage maybe
@@ -30,6 +28,11 @@ function App() {
   function handleDelete(id: string) {
     setTimers((prev) => prev.filter((t) => t.id !== id));
   }
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter" || e.key === "NumpadEnter") {
+      handleAdd();
+    }
+  }
   return (
     <main className="flex flex-col gap-4 items-center h-screen bg-black text-white p-4">
       <label className="flex gap-2 items-center" htmlFor="timerLength">
@@ -39,6 +42,7 @@ function App() {
           type="number"
           name="timerLength"
           className="text-slate-500 rounded-sm"
+          onKeyDown={handleKeyDown}
         />
         <button onClick={handleAdd} className="rounded bg-slate-700 px-4 py-2">
           Add
